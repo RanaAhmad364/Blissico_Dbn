94edaaa1951fd2e014c244d812da195b9343d887
Ahmad <ra7514770@gmail.com>
2026-09-02 23:06:54 -0700

    Update axiosConfig.js

--- blissico_app/src/api/axiosConfig.js
+++ blissico_app/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://127.0.0.1:5000';
+// export const BASE_URL = 'http://127.0.0.1:5000';
+export const BASE_URL = 'https://blissico-dbn.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
